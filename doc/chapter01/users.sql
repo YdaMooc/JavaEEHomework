2fedeb14979eec0f854307d5867bcea8d7d668a5
YdaMooc <3156266991@qq.com>
2024-11-24 01:25:08 +0800

    补充了chapter01的mysql数据库表文件

---
+++ doc/chapter01/users.sql
@@ -0,0 +1,38 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 达
+ Source Server Type    : MySQL
+ Source Server Version : 80033
+ Source Host           : localhost:3306
+ Source Schema         : javaee
+
+ Target Server Type    : MySQL
+ Target Server Version : 80033
+ File Encoding         : 65001
+
+ Date: 24/11/2024 01:23:49
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `uid` int(0) NOT NULL AUTO_INCREMENT,
+  `uname` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `uage` int(0) NOT NULL,
+  PRIMARY KEY (`uid`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of users
+-- ----------------------------
+INSERT INTO `users` VALUES (1, '张三', 20);
+INSERT INTO `users` VALUES (2, '李四', 18);
+INSERT INTO `users` VALUES (3, '王五', 22);
+
+SET FOREIGN_KEY_CHECKS = 1;
