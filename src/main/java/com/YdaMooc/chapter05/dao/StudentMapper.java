2f7337d2671cf4def84f9a4b080e0ce1761e6aa8
YdaMooc <3156266991@qq.com>
2024-11-29 20:07:36 +0800

    1. 修改了README文档，标记chapter05已完成 2. 补充了chapter05作业中未使用到的findStudentByClassId()

--- src/main/java/com/YdaMooc/chapter05/dao/StudentMapper.java
+++ src/main/java/com/YdaMooc/chapter05/dao/StudentMapper.java
@@ -4,9 +4,11 @@ import com.YdaMooc.chapter03.pojo.Student;
 import com.YdaMooc.chapter05.pojo.Students;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.List;
+
 public interface StudentMapper {
-    @Select("SELECT * FROM t_students_2 WHERE cid=#{id}")
-    Students findStudentByClassId(int cid);
+    @Select("SELECT * FROM t_students_2 WHERE cid=#{cid}")
+    List<Students> findStudentByClassId(int cid);
 
     @Select("SELECT * FROM t_students_2 WHERE id=#{id}")
     Students selectStudent(int id);
--- src/test/java/com/YdaMooc/chapter05/StudentsTest.java
+++ src/test/java/com/YdaMooc/chapter05/StudentsTest.java
@@ -2,13 +2,15 @@ package com.YdaMooc.chapter05;
 
 import com.YdaMooc.chapter05.dao.ClassMapper;
 import com.YdaMooc.chapter05.dao.StudentMapper;
+import com.YdaMooc.chapter05.pojo.Class;
 import com.YdaMooc.chapter05.pojo.Students;
 import com.YdaMooc.chapter05.utils.JDBCUtils;
-import com.YdaMooc.chapter05.pojo.Class;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 public class StudentsTest {
     private SqlSession sqlSession = null;
@@ -22,8 +24,8 @@ public class StudentsTest {
     @Test
     public void findStudentByIdTest() {
         StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
-        Students students = mapper.selectStudent(2);
-        System.out.println(students.toString());
+        Students student = mapper.selectStudent(2);
+        System.out.println(student.toString());
     }
 
     // 查询班级信息，一对多查询班级学生信息
@@ -32,6 +34,14 @@ public class StudentsTest {
         ClassMapper mapper = sqlSession.getMapper(ClassMapper.class);
         Class cls = mapper.selectClassById(2);
         System.out.println(cls.toString());
+    }
+
+    // 根据班级id查找对应学生
+    @Test
+    public void findStudentByClassIdTest() {
+        StudentMapper mapper = sqlSession.getMapper(StudentMapper.class);
+        List<Students> student = mapper.findStudentByClassId(2);
+        System.out.println(student.toString());
     }
 
     @After
